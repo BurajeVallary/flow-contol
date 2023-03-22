04f0ff9c9b793e47398859eb01ba291a063a7458
Vallary Buraje <vallaryburaje@gmail.com>
2023-03-22 17:28:28 +0300

    corrections

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,6 +9,7 @@ oddNumbers()
 
 
 }
+// 1 Create a function that prints out all the odd numbers between 1 and 100
 fun oddNumbers(){
     for (b in 1..100){
      if (b % 2!=0){
@@ -16,6 +17,8 @@ fun oddNumbers(){
      }
     }
 }
+//2 Create a function that takes in an array of names and returns the number of
+//names longer than 5 characters
 fun  words(names:Array<String>):Int{
     var add=0
     for (name in names ){
@@ -25,14 +28,24 @@ fun  words(names:Array<String>):Int{
     }
 return  add
 }
+// 3 You are creating a robot to serve drinks at a party. The robot is required to
+//serve a glass of milk for guests under the age of 6. For guests under the age of
+//15 but over the age of 6, the robot serves a bottle of fanta orange. Everybody
+//else gets a bottle of cocacola. Create a function that will make these
+//decisions, printing out the name of the drink each person gets according to
+//their age.
 
 fun serveDrinkDifferrent(age: Int) {
      when (age) {
         in 0..5 ->  println("glass of milk")
-        in 6..14 -> println("bottle of fanta Orange")
+        in 7..14 -> println("bottle of fanta Orange")
         else -> println ("bottle of coca cola")
     }
 }
+//Write a function that prints each number from 1 to 100 on a new line. For each
+//multiple of 3, print "Fizz" instead of the number. For each multiple of 5, print
+//"Buzz" instead of the number. For numbers which are multiples of both 3 and
+//5, print "FizzBuzz" instead of the number.
 fun buzz(){
   for (item in 1..100){
        if(item %3==0&&item%5==0){
